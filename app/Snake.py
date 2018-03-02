92efc97c443d2b25fcabadbde033f38fb78e8655
Dragon Prevost <dragon@dkp.io>
2018-03-02 14:00:34 -0800

    fixed snake class

--- app/Snake.py
+++ app/Snake.py
@@ -1,7 +1,5 @@
 class Snake:
     def __init__(self, data):
-        self.x = data['body']['data'][0]['x']
-        self.y = data['body']['data'][0]['y']
         self.head = (  data['body']['data'][0]['x']  ,  data['body']['data'][0]['y']  )
         self.health = data['health']
         self.length = len(data['body']['data'])
