eee53ec69bb57e88736567413958f0db5bf2427e
Dragon Prevost <dragon@dkp.io>
2018-03-03 10:28:15 -0800

    changing astar iimprt name again

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from Node import Node
 from Board import Board
 from Food import Food
 from Snake import Snake
-from astar import aStar, getNeighbours
+from aStar import aStar, getNeighbours
 
 @bottle.route('/')
 def static():
