70bfb826220689d8e39cd4e3551247bf32bf22d7
Goh Sato <gsato@uvic.ca>
2019-02-27 19:12:53 -0800

    updated to engine0.2.18 Windows

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import bottle
+from bottle import HTTPResponse
 import os
 import random
 import json
@@ -63,7 +64,7 @@ def move():
     direction = ""
     if path is None or snake.length < 2 or data['turn'] < 2:
         neighbours = get_neighbours(snake.get_head().get_point(), board, FoodFilter())
-        if len(neighbours) < 0:
+        if len(neighbours) <= 0:
             return "down"
         direction = dir(snake.get_head(), neighbours[0])
     else:
@@ -71,11 +72,32 @@ def move():
     
     print direction
 
-    return {
+    return HTTPResponse(
+        status = 200,
+        headers={
+            "Content-Type": "application/json"
+        },
+        body=json.dumps({
         'move': direction,
         'taunt': 'battlesnake-python!'
-    }
+        })
+    )
 
+@bottle.post('/ping')
+def ping():
+    return bottle.HTTPResponse(status=200)
+
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
+
+    """
+    TODO: If your snake AI was stateful,
+        clean up any stateful objects here.
+    """
+    print(json.dumps(data))
+
+    return HTTPResponse(status=200)
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
@@ -85,4 +107,5 @@ if __name__ == '__main__':
         application,
         host=os.getenv('IP', '0.0.0.0'),
         port=os.getenv('PORT', '8080'),
-        debug = True)
+        debug = os.getenv('DEBUG',True)
+    )
