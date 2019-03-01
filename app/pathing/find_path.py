e9467d73eb055bf7635bbe170271f7d729b47a27
dragonprevost <dragon@dkp.io>
2019-03-01 11:56:14 -0800

    fixed bug

--- app/pathing/find_path.py
+++ app/pathing/find_path.py
@@ -7,7 +7,7 @@ TURNS_TO_CHASE_FOOD = 50
 HEALTH_TO_CHASE_FOOD = 50
 
 # Follows 'General Tree' diagram
-def find_path(board):
+def find_path_new(board):
     food = board.get_food_list()
     
     emerge_checks(board)
@@ -33,4 +33,27 @@ def find_path(board):
     return move
 
 
+def find_path(board):
+    food_list = board.get_food_list()
+    us = board.get_our_snake()
+ 
+    if( us.health > HEALTH_TO_CHASE_FOOD and board.get_turn() > TURNS_TO_CHASE_FOOD):
+        return stall(board)
+
+    fpath = food_path(food_list,board)
+    if not fpath:
+            return stall(board)
+    fpath.reverse()
+
+
+    while not is_viable(board,fpath) :
+        idx = food_list.index(fpath[-1])
+        if idx >= len(food_list): return stall(board)
+        else: food_list = food_list[idx+1:]
+        fpath = food_path(food_list,board)
+        if not fpath:
+            return stall(board)
+        fpath.reverse()
+        
+    return fpath
 
