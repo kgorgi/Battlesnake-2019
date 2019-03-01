d239f34e006fa4ed473685e529452467946e5cfa
dragonprevost <dragon@dkp.io>
2019-03-01 14:05:35 -0800

    updated tail following decision logic

--- app/pathing/find_path.py
+++ app/pathing/find_path.py
@@ -6,37 +6,11 @@ from util_path import is_viable, emerge_checks
 TURNS_TO_CHASE_FOOD = 50
 HEALTH_TO_CHASE_FOOD = 50
 
-# Follows 'General Tree' diagram
-def find_path_new(board):
-    food = board.get_food_list()
-    
-    emerge_checks(board)
-
-    if(board.get_turn() < TURNS_TO_CHASE_FOOD or 
-        board.get_our_snake() < HEALTH_TO_CHASE_FOOD):
-        move = find_food(board,food)
-    
-    if(move is None):
-        return stall(board)
-
-    # safety_check(move):
-
-    while not is_viable(board,move) :
-        idx = food.index(move[-1])
-        if idx >= len(food): return stall(board)
-        else: food = food[idx+1:]
-        move = find_food(board, food)
-        if not move:
-            return stall(board)
-        move.reverse()
-    
-    return move
-
-
 def find_path(board):
     food_list = board.get_food_list()
     us = board.get_our_snake()
- 
+
+    # If Checking for Stall Scenarios 
     if( us.health > HEALTH_TO_CHASE_FOOD and board.get_turn() > TURNS_TO_CHASE_FOOD):
         return stall(board)
 
--- app/pathing/stall_path.py
+++ app/pathing/stall_path.py
@@ -1,12 +1,38 @@
 from tail_path import path_to_tail
+from filters import SnakePartFilter
+from astar import aStar
 
+# General Decision Logic Exists Here
 def stall(board):
-    
     us = board.get_our_snake()
-    path= path_to_tail(board)
+    path = follow_our_tail(board)
     
-    if path and board.is_snake_node(path[-1]) and path[-1].get_snake_info().health == 100 and len(path)<=2:
-        return None
-
-
+    # If no path to tail follow other tail
+    if path is None:
+        path = follow_other_tail(board)   
+    
+    # If no path to other tail fill board
+    if path is None:
+        path = fill(board)
     return path
+
+# TODO
+# Return path to fill board not following tail
+def fill(board):
+    return None
+
+# Returns path to our tail
+def follow_our_tail(board):
+    our_tail = [ board.get_our_snake().get_tail() ]
+    head = board.get_our_snake().get_head()
+    path_to_tail = aStar(head, our_tail, board, SnakePartFilter(our_tail))
+    return path_to_tail
+
+# Return path to other tail
+def follow_other_tail():
+    snake_tails = [info.get_tail() for info in board.get_enemies()]
+    head = board.get_our_snake().get_head()
+    path_to_tail = aStar(head, snake_tails, board, SnakePartFilter(snake_tails))
+    return path_to_tail(board)
+
+
