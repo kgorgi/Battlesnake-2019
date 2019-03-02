d246010d9541c7ac164ae05249c7dabaebc9f717
dragonprevost <dragon@dkp.io>
2019-03-01 22:34:26 -0800

    attack function is working

--- app/pathing/attack_path.py
+++ app/pathing/attack_path.py
@@ -4,23 +4,25 @@ from util_path import is_viable
 from neighbours import get_neighbours
 
 def attack(board):
-    return None
     us = board.get_our_snake()
     snake_heads = [info.get_head() for info in board.get_enemies()]
     snake_heads.append(board.get_our_snake().get_head())
     # Get Possible moves of enemies head
     # Select move closest to center 
     # A star to that position
-    print 'attacking'
     enemy_moves = find_enemy_moves(board)
     attack_point = closest_to_center(board, enemy_moves)
-    print 'attacking: ', attack_point
+    
+    path = None
+
     # If we can attack, return path to attack point
     if attack_point: 
         print 'returning attack path'
-        return aStar(us.get_head(), attack_point, board, SnakePartFilter(snake_heads))
-
-    return None
+        print us.get_head().get_point()
+        path = aStar(attack_point, [ us.get_head() ], board, SnakePartFilter( [ us.get_head() ] ))
+    
+    
+    return path
 
 # Return move an enemy can make closes to middle of board
 def find_enemy_moves(board):
@@ -32,23 +34,19 @@ def find_enemy_moves(board):
     return moves
 
 def closest_to_center(board, moves):
-    print moves
     if not moves:   
         return None
 
     center = ( board.get_width() / 2 , board.get_height() / 2 )
-    print 'center: ', center
     closest = None 
     manhattan = board.get_width() + board.get_height()
     for move in moves:
-        print move.get_point()
         dist = manhattan_dist(move.get_point(), center)
         if dist < manhattan:
             closest = move
             manhattan = dist
     
-    print 'closest: ', closest.get_point()
-    return [ closest ]
+    return  closest 
 
 def manhattan_dist(pointA, pointB):
     return abs(pointA[0] - pointB[0]) + abs(pointA[1] - pointB[1])
