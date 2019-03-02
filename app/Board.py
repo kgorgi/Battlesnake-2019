3318b2d807f51b79cbdee11743666e3b0009c880
baraka <blwakila@gmail.com>
2019-03-02 12:29:24 -0800

    just a test

--- app/Board.py
+++ app/Board.py
@@ -27,6 +27,7 @@ class Board:
                 self._enemy_list.append(snake_info)
                 head = snake['body'][0]
                 for op in [(1,1),(-1,1),(1,-1),(-1,-1)]:
+                    print "HEAD " + head + " OP " + op
                     px = head[0]+op[0]
                     py = head[1]+op[1]
                     if px > self.get_width or px < 0 or py < 0 or py > self.get_height:
