379f220d4277416cfb3555383cce8ef2c90eda34
dragonprevost <dragon@dkp.io>
2019-03-01 16:09:07 -0800

    initialized attack file

---
+++ app/pathing/attack_path.py
@@ -0,0 +1,17 @@
+from tail_path import path_to_tail
+from filters import SnakePartFilter
+from astar import aStar
+
+def attack(board):
+    # Get Possible moves of enemies head
+    # Select move closest to center 
+    # A star to that position
+    print 'attacking'
+    return None
+
+# Return move an enemy can make closes to middle of board
+def find_enemies_moves(board):
+    enemy = board.get_enemies()[0]
+    print enemy
+    pass
+
